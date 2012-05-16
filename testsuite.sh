a4681ca4b9f5cb2844af328306cc48567be24c03
Christophe Lincoln <pankso@slitaz.org>
2012-05-16 16:55:32 +0200

    Add libtaz.sh and libpkg.sh functions usage to testsuite

--- testsuite.sh
+++ testsuite.sh
@@ -22,6 +22,31 @@ check_libtaz() {
 	separator
 }
 
+# Usage: check_functions path/to/lib.sh
+check_functions() {
+	lib=$1
+	echo -n "$(boldify "Checking: $(basename $lib) functions")"
+	indent 34 "$(colorize $(grep "[a-z]() {" $lib | wc -l) 32)"
+	separator
+	grep "[a-z]() {" $lib | while read line
+	do
+		func=`echo "$line" | cut -d '(' -f 1`
+		count=0
+		usage=0
+		echo -n "Checking: ${func}()"
+		for tool in /usr/bin/cook* /usr/bin/taz* /usr/bin/spk* /usr/sbin/spk* \
+			/sbin/taz*
+		do
+			[ -x "$tool" ] || continue
+			count=$(grep "$func" $tool | wc -l)
+			usage=$(($usage + $count))
+		done
+		indent 34 "$usage"
+	done
+	separator
+}
+
+#clear
 echo -n "Checking libtaz.sh: log()"
 activity=/tmp/testsuite.log
 log "Message from SliTaz testsuite"
@@ -31,10 +56,17 @@ rm -f $activity
 
 check_libtaz
 output="raw"
+echo "Checking libtaz.sh: --output=raw"
 check_libtaz
+
+# Check libtaz.sh functions usage
+output="term"
+check_functions 'rootfs/lib/libtaz.sh'
+
+# Check libpkg.sh functions usage
+check_functions 'rootfs/usr/lib/slitaz/libpkg.sh'
 
 [ "$forced" ] && echo "Checking option: forced=$forced"
 [ "$root" ] && echo "Checking option: root=$root"
 [ ! "$1" ] && echo "Check options: $(basename $0) --forced --root=/dev/null"
-
 exit 0
