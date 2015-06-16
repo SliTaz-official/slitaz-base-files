27e00f9eb15dbd33810d1f3fdbad82acaed67b5b
Aleksej Bobylev <al.bobylev@gmail.com>
2015-06-16 20:47:25 +0300

    libtaz.sh: do not fail processing --options-with-dashes or --weird%option$. Add custom tag "x-details" support to slitaz-doc.{css,js}

--- rootfs/lib/libtaz.sh
+++ rootfs/lib/libtaz.sh
@@ -28,10 +28,12 @@ ermsg="$(lgettext 'Failed')"
 
 # Parse cmdline options and store values in a variable.
 for opt in "$@"; do
-	case "$opt" in
-		--*=*) export "${opt#--}";;
-		--*)   export  ${opt#--}='yes';;
+	opt_name="${opt%%=*}"; opt_name="$(echo "${opt_name#--}" | tr -c 'a-zA-Z0-9' '_')"
+	case "$opt_name" in
+		[0-9]*) opt_name="_$opt_name";;
 	esac
+	opt_value="${opt#*=}"; opt_value="${opt_value:-yes}"
+	export "$opt_name=$opt_value"
 done
 [ "$HTTP_REFERER" ] && output='html'
 
--- rootfs/usr/share/doc/slitaz-doc.js
+++ rootfs/usr/share/doc/slitaz-doc.js
@@ -5,4 +5,15 @@ window.onload = function() {
 		var element = preElements[i];
 		element.innerHTML = element.innerHTML.replace(/^#/gm,'<tt>#</tt>').replace(/^\$/gm,'<tt>$</tt>');
 	}
+
+	// <details> html element not supported in the current SliTaz gtk-webkit
+	// use custom <x-details> instead
+	var detailsElements = document.getElementsByTagName('x-details');
+	for(var i = 0; i < detailsElements.length; ++ i)
+	{
+		var element = detailsElements[i];
+		element.innerHTML = '<input type="checkbox" id="details' + i +
+			'"><label for="details' + i + '"></label><span>' +
+			element.innerHTML + '</span>';
+	}
 }
