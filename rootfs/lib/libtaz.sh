c9a6a4ff97301b5adb79e3fcd24e6631dccdc7b6
Aleksej Bobylev <al.bobylev@gmail.com>
2015-06-18 19:58:55 +0300

    libtaz.sh: fix last commit (again) thanks Pascal

--- rootfs/lib/libtaz.sh
+++ rootfs/lib/libtaz.sh
@@ -28,12 +28,16 @@ ermsg="$(lgettext 'Failed')"
 
 # Parse cmdline options and store values in a variable.
 for opt in "$@"; do
-	opt_name="${opt%%=*}"; opt_name="$(echo -n "${opt_name#--}" | tr -c 'a-zA-Z0-9' '_')"
-	case "$opt_name" in
-		[0-9]*) opt_name="_$opt_name";;
+	case "$opt" in
+		--*)
+			opt_name="${opt%%=*}"; opt_name="$(echo -n "${opt_name#--}" | tr -c 'a-zA-Z0-9' '_')"
+			case "$opt_name" in
+				[0-9]*) opt_name="_$opt_name";;
+			esac
+			opt_value="${opt#--}="; opt_value="${opt_value#*=}"; opt_value="${opt_value%=}"
+			export "$opt_name=${opt_value:-yes}"
+			;;
 	esac
-	opt_value="${opt#--}="; opt_value="${opt_value#*=}"; opt_value="${opt_value%=}"
-	export "$opt_name=${opt_value:-yes}"
 done
 [ "$HTTP_REFERER" ] && output='html'
 
